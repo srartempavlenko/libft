12a3794e191fd1e7865f8758a4297ed6bf34ab74
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-01 23:53:13 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -1,8 +1,13 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
 #include <stdlib.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
+int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isalnum(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
 size_t  ft_strlen(const char *s);
+
+#endif
