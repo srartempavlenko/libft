76f68e9a961b8d22a483171410ed6e94085fc030
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-02 23:58:15 +0200

    Update libft.h

--- libft.h
+++ libft.h
@@ -9,5 +9,11 @@ int     ft_isalnum(int c);
 int     ft_isascii(int c);
 int     ft_isprint(int c);
 size_t  ft_strlen(const char *s);
+void    *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
