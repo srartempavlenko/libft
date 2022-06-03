42a2bda49098bfd41b6926cb13122f6896ef4d7e
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-03 19:19:07 +0200

    Update libft.h and add ft_tolower.c

--- libft.h
+++ libft.h
@@ -16,5 +16,6 @@ void    *ft_memmove(void *dst, const void *src, size_t len);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 int     ft_toupper(int c);
+int     ft_tolower(int c);
 
 #endif
