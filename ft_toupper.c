ea40e25017de12b41342c819e166da7ca63cfae3
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-03 19:04:07 +0200

    Update libft.h and add ft_toupper.c

---
+++ ft_toupper.c
@@ -0,0 +1,10 @@
+int ft_toupper(int c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        c -= 32;
+        return (c);
+    }
+    else
+        return (c);
+}
--- libft.h
+++ libft.h
@@ -15,5 +15,6 @@ void    *ft_memcpy(void *dst, const void *src, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
 size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+int     ft_toupper(int c);
 
 #endif
