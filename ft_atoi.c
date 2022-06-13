4c41c604b861e2233d3cb742f016293eda0edec2
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-13 17:28:55 +0200

    Add ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,25 @@
+int ft_atoi(const char *str)
+{
+    int i;
+    int sig;
+    int res;
+    
+    i = 0;
+    while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+        i++;
+    sig = 0;
+    if (str[i] == '+')
+        i++;
+    else if (str[i] == '-')
+    {
+        sig = -1;
+        i++;
+    }
+    res = 0;
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        res = (res * 10) + (str[i] - '0');
+        i++;
+    }
+    return (sig * res);
+}
