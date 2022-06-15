511df72e7f5d303acb766d4307bc0e158226de1b
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-15 19:36:47 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -2,10 +2,10 @@
 
 size_t  ft_strlen(const char *s)
 {
-    size_t  len;
+    size_t  i;
 
-    len = 0;
-    while (s[len])
-        len++;
-    return (len);
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
 }
