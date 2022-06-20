528c676c39ee04148521d429b1c006c22f049584
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-20 16:17:29 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,12 +5,12 @@ void    *ft_memcpy(void *dst, const void *src, size_t n)
     size_t  i;
     unsigned char   *d;
     unsigned char   *s;
-
-    d = (unsigned char *)dst;
-    s = (unsigned char *)src;
+    
     if (!dst || !src)
         return (NULL);
     i = 0;
+    d = (unsigned char *)dst;
+    s = (unsigned char *)src;
     while (i++ < n)
         *(d++) = *(s++);
     return (dst);
