3135ec07162851fbcd29ced4f987394f0253c16d
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-28 18:28:28 +0200

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -2,10 +2,5 @@
 
 void    *ft_memset(void *b, int c, size_t len)
 {
-    size_t  i;
-
-    i = 0;
-    while (i++ < len)
-        *((unsigned char *)(b + i)) = (unsigned char)c;
-    return (b);
+    
 }
