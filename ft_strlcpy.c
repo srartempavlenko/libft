32f16cd401f728b8f09ff53145a9bc684ca9caa4
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-06-29 14:41:36 +0200

    Update ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+    size_t  d;
+    size_t  s;
+    size_t  i;
+
+    d = 0;
+    s = 0;
+    while (dst[d] != '\0')
+        d++;
+    while (src[s] != '\0')
+        s++;
+    if (dstsize == 0)
+        return (s);
+    i = 0;
+    while ((src[i] != '\0') && (i < (dstsize - 1)))
+    {
+        dst[i] == src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (s);
+}
