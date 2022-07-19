62f9fb8a60ec23d712091025016bbe66b6fff091
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-19 14:16:05 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,17 +1,18 @@
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    size_t  i;
-    unsigned char   *d;
-    unsigned char   *s;
-    
-    if (!dst || !src)
-        return (NULL);
-    i = 0;
-    d = (unsigned char *)dst;
-    s = (unsigned char *)src;
-    while (i++ < n)
-        *(d++) = *(s++);
-    return (dst);
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
 }
