4142bd53bfc702f8154c6cf70a9c05693faeb36a
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-19 18:53:36 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,27 +1,26 @@
 #include "libft.h"
 
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t  j;
-    size_t  i;
-    size_t  d;
-    size_t  s;
+	size_t	d;
+	size_t	s;
+	size_t	j;
+	size_t	i;
 
-    j = ft_strlen(dst);
-    i = 0;
-    d = ft_strlen(dst);
-    s = ft_strlen(src);
-    if (dstsize < 1)
-        return (s + dstsize);
-    while ((src[i] != '\0') && (i < (dstsize - 1)))
-    {
-        dst[j] = src[i];
-        j++;
-        i++;
-    }
-    dst[j] = '\0';
-    if (dstsize < d)
-        return (s + dstsize);
-    else
-        return (d + s);
+	d = ft_strlen(dst);
+	s = ft_strlen(src);
+	j = d;
+	i = 0;
+	if (dstsize != 0 && dstsize > d)
+	{
+		while (src[i] && (i < (dstsize - 1)))
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = '\0';
+		return (d + s);
+	}
+	return (s + dstsize);
 }
