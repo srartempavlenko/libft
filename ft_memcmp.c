33a02c9d9b7d091452311271e84a553fbf0567d1
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-20 13:40:47 +0200

    ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*a;
+	char	*b;
+	size_t	i;
+
+	a = (char *)s1;
+	b = (char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (a[i] != b[i])
+			return ((unsigned char)a[i] - (unsigned char)b[i]);
+		i++;
+	}
+	return (0);
+}
