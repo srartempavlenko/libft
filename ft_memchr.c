019da07d4eb5d6c2f45c2716a9b5697078202c7c
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-20 13:25:31 +0200

    Update ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char)str[i] == (unsigned char)c)
+			return ((char *)s[i]);
+		i++;
+	}
+	return (NULL);
+}
