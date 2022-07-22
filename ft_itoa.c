36ad45e7d3417f4896435347b0c107e3e75b034f
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-22 11:27:48 +0200

    Add ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+char	*ft_itoacpy(char *dst, const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+		dst[i] = '\0';
+	}
+	return (dst);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * 2);
+	if (str == NULL)
+		return (NULL);
+	if (n == -2147483648)
+		return (ft_itoacpy(str, "-2147483648"));
+	if (n < 0)
+	{
+		str[0] = '-';
+		str[1] = '\0';
+		str = ft_strjoin(str, ft_itoa(-n));
+	}
+	else if (n >= 10)
+		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
+	else if (n < 10 && n >= 0)
+	{
+		str[0] = n + '0';
+		str[1] = '\0';
+	}
+	return (str);
+}
