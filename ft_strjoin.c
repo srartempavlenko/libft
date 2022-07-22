c380eb19e14ea5bee4bbbb232236bf3139f70e74
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-22 11:24:39 +0200

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len1;
+	int		len2;
+	char	*str;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) + (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, len1 + 1);
+	ft_strlcat(str, s2, len1 + len2 + 1);
+	return (str);
+}
