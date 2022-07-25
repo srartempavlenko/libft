efbdf64e287aaab1071b47660277ca6cfd9b8254
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:35:24 +0200

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,13 +2,22 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*str;
+	unsigned char	*dststr;
+	unsigned char	*srcstr;
 
-	str = (unsigned char *)malloc(sizeof(unsigned char) * len);
-	if (str == NULL)
+	if (dst == NULL && src == NULL)
 		return (NULL);
-	ft_memcpy(str, src, len);
-	ft_memcpy(dst, str, len);
-	free(str);
+	dststr = (unsigned char *)dst;
+	srcstr = (unsigned char *)src;
+	if (dst > src)
+	{
+		while (len)
+		{
+			*(dststr + len - 1) = *(srcstr + len - 1);
+			len--;
+		}
+	}
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
