d1030d96d939327151191a58ee4ba8ade670a9f4
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:37:41 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@ char	*ft_strdup(const char *s1)
 	char	*str;
 
 	len = ft_strlen(s1) + 1;
-	str = malloc(sizeof(char) + len);
+	str = (char *)malloc(sizeof(char) * len);
 	if (str == NULL)
 		return (NULL);
 	ft_memcpy(str, s1, len);
