4bc1d38353f6f3a5052cfd8fd83d5e206cb407cd
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:40:21 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -4,10 +4,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	i = 0;
 	if (n == 0)
 		return (0);
-	while ((s1[i]) && (s1[i] == s2[i]) && (i < n))
+	i = 0;
+	while (i < n && (*(s1 + i) != '\0' || *(s2 + i) != '\0'))
+	{
+		if (*(s1 + i) != *(s2 + i))
+			return ((unsigned char)*(s1 + i) - (unsigned char)*(s2 + i));
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
