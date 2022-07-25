9cdbcee0aab097c984d9e2bc30e5e93b54a50d49
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:44:46 +0200

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -3,17 +3,16 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
-	unsigned int	i;
+	unsigned int	slen;
 
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	if ((slen - start) < len)
+		len = slen - start;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
+	ft_strlcpy(str, (char *)(s + start), len + 1);
 	return (str);
 }
