fc08ab7719073da87fedcc362bdc4a2828038a16
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:40:47 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,20 +1,21 @@
-#include	"libft.h"
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	j;	
+	size_t	j;
 
-	if (!*(needle))
+	if (!(*needle))
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] && i < len)
+	while (*(haystack + i) && i < len)
 	{
 		j = 0;
-		while ((neddle[j]) && (haystack[i + j] == neddle[j]) && ((i + j) < len))
+		while (*(needle + j) && ((*(haystack + i + j) == *(needle + j)))
+			&& (i + j) < len)
 			j++;
-		if (!neddle[j])
-			return ((char)haystack[i]);
+		if (!(*(needle + j)))
+			return ((char *)(haystack + i));
 		i++;
 	}
 	return (NULL);
