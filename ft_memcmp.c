dfa94652985a6dc0aee579a2436579a09349f860
srartempavlenko <96959642+srartempavlenko@users.noreply.github.com>
2022-07-25 23:34:01 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,17 +2,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*a;
-	char	*b;
+	char	*ststr;
+	char	*ndstr;
 	size_t	i;
 
-	a = (char *)s1;
-	b = (char *)s2;
+	ststr = (char *)s1;
+	ndstr = (char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if (a[i] != b[i])
-			return ((unsigned char)a[i] - (unsigned char)b[i]);
+		if (*(ststr + i) != *(ndstr + i))
+			return ((unsigned char)*(ststr + i)
+				- (unsigned char)*(ndstr + i));
 		i++;
 	}
 	return (0);
